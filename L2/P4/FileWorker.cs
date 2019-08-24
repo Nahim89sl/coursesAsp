e160b0cdec3d26de2252b98fa0763513a3612de9
Nahim89sl <nahim89sl@gmail.com>
2019-08-24 15:10:42 +0300

    First Commit

---
+++ L2/P4/FileWorker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WebServer
+{
+    public class FileWorker
+    {
+        private static string ErrorNotFindSite = "<html><head><title>Site ## not exist </title> <meta charset='utf8'></head><body>Sorry! We can't find web-site ## </body></html>";
+        private static string ErrorNotFindPage = "<html><head><title>Page ## not finded </title> <meta charset='utf8'></head><body>Maybe page ## not exist on this site</body></html>";
+        private static string ErrorwrongRequest = "<html><head><title>WrongRequest</title> <meta charset='utf8'></head><body>Your request is ##<br>Try check it</body></html>";
+        private static string portSrv;
+        private static string webSrvFolder;
+
+        public FileWorker(string port,string serverFolder)
+        {
+            portSrv = port;
+            webSrvFolder = serverFolder;
+        }
+
+
+
+        public string AnswerToRequest(string request)
+        {
+            //try finde site name
+            Console.WriteLine($"Request: {request}");
+            Regex siteRgx = new Regex("(?<=:"+ portSrv + "/).*?(?=/)");
+            Match match = siteRgx.Match(request);
+            if(match.Success)
+            {
+                string siteName = match.Value;
+                if(Directory.Exists(webSrvFolder+siteName))
+                {
+                    Console.WriteLine($"site: {siteName}");
+                    //try find file from request
+                    Regex fileRgx = new Regex("(?<="+ siteName+ "/).*");
+                    match = fileRgx.Match(request);
+                    string filePath;
+                    if (match.Success)
+                    {
+                        filePath = webSrvFolder + siteName + "\\" + match.Value;
+                        Console.WriteLine($"File name: {match.Value}");
+                    }
+                    else
+                    {
+                        filePath = webSrvFolder + siteName + "\\index.html";
+                    }
+                    
+                    //try find file on server
+                    if (File.Exists(filePath))
+                    {
+                        Console.WriteLine($"Return answer");
+                        string fileContent = File.ReadAllText(filePath);
+                        return fileContent;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Page not find: {match.Value}");
+                        return ErrorNotFindPage.Replace("##", match.Value);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Site {siteName} not finded");
+                    return ErrorNotFindSite.Replace("##", siteName);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Wrong request {request}");
+                return ErrorwrongRequest.Replace("##", request);
+            }
+        }
+    }
+}
---
+++ L2/P4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string port = "8881";
+            string serverFolder = "C:\\Users\\Administrator\\source\\repos\\coursesAsp\\L2\\sites\\";
+
+            FileWorker fileAnswer = new FileWorker(port, serverFolder);
+
+            HttpListener listner = new HttpListener();
+            listner.Prefixes.Add("http://*:"+port+"/");
+            listner.Start();
+            Console.WriteLine("Web Server wait connections on port:"+port);
+            while (true)
+            {
+                HttpListenerContext context = listner.GetContext();
+                HttpListenerRequest request = context.Request;
+                if (request.Url.ToString().Contains("STOP"))
+                {
+                    listner.Stop();
+                    Console.WriteLine("Stop listner");
+                    break;
+                }
+                HttpListenerResponse responce = context.Response;//wait request
+
+                string responceStr = fileAnswer.AnswerToRequest(request.Url.ToString());
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responceStr);
+                responce.ContentLength64 = buffer.Length;
+                Stream output = responce.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
