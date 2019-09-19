d623a041ecc657017368bce727cce9356e4157b5
Nahim89sl <nahim89sl@gmail.com>
2019-09-19 18:22:05 +0300

    commiyt 2

---
+++ L2/L4-P2 MVC-IoC-AutofacWithFileUpload/L4-P1-5/Controllers/ParticipantController.cs
@@ -0,0 +1,32 @@
+﻿using L4_P1_5.DAL;
+using L4_P1_5.Infrastructure;
+using L4_P1_5.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L4_P1_5.Controllers
+{
+    public class ParticipantController : Controller
+    {
+        // GET: Participant
+        private IParticipantsRepository ParticipantsService { get; }
+        private ILogger Logger { get; }
+
+        public ParticipantController(IParticipantsRepository participantsRepository, ILogger logger)
+        {
+            ParticipantsService = participantsRepository;
+            Logger = logger;
+        }
+        public ActionResult Index()
+        {
+            if (RouteData.Values["name"] != null)
+            {
+                var participant = ParticipantsService.
+            }
+            return View();
+        }
+    }
+}
---
+++ L2/L4-P2 MVC-IoC-AutofacWithFileUpload/L4-P1-5/Models/Participant.cs
@@ -0,0 +1,18 @@
+﻿namespace L4_P1_5.Models
+{
+    public class Participant
+    {
+        public int PartyId { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public bool IsAttend { get; set; }
+
+        public Participant(int partyId, string name, bool isAttend, string avatar)
+        {
+            PartyId = partyId;
+            Name = name;
+            IsAttend = isAttend;
+            Avatar = avatar;
+        }
+    }
+}
