0e6835087731952396b39bbafc5e3407ce35b22d
admin <admin@DESKTOP-PTKBMBD>
2019-09-24 18:14:39 +0300

    coockie
    cookie

---
+++ L2/L4-P2 MVC-IoC-AutofacWithFileUpload/L4-P1-5/ViewModel/ParticipantView.cs
@@ -0,0 +1,17 @@
+﻿using L4_P1_5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L4_P1_5.ViewModel
+{
+    public class ParticipantView
+    {
+        public Participant User { get; set; }
+        public ParticipantView(Participant user)
+        {
+            User = user;
+        }
+    }
+}
